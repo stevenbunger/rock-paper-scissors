f24d6ce706ad8ae638fc46c6ede9dbfab34fbfc4
stevenbungergit config --global user.email steven@buenger-web.de <stevenbunger@Stevens-MacBook-Pro.local>
2024-05-31 15:03:41 +0200

    rock apper scissors game

---
+++ script.js
@@ -0,0 +1,86 @@
+
+function getHumanChoice (choices) {
+    let humanInputChoice = prompt("Enter your Choice (rock, paper or scissors): ");
+    return humanInputChoice;
+}
+
+
+// function getComputerChoice (choices) {
+//     const randomChoice = Math.floor(Math.random() * choices.length);
+//     const randomElement = choices[randomChoice];
+//     return randomElement;
+// };    
+
+
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+    let roundsPlayed = 0;
+
+    const maxRounds = 5;
+
+    function playRound(humanSelection, computerSelection) {
+
+        const choices = ["rock", "paper", "scissors"];
+
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        const computerSelection = choices[randomIndex];
+
+            const outcome = {
+                rock: {rock: "It's a tie!", paper: "You lose! Paper beats rock!", scissors: "You win! rock beats scissors"},
+                paper: {rock: "You win! paper beats rock!", paper: "It's a tie!!", scissors: "You lose! scissors beats paper"},
+                scissors: {rock: "You lose! Rock beats scissors", paper: "You win! Scissors beat paper", scissors: "It's a tie!"}
+            };
+
+            const outcomeMessage = outcome[humanSelection] && outcome[humanSelection][computerSelection];
+
+            const result = {
+                message: outcomeMessage || "Invalid Selection",
+                winner: ""
+            }
+
+
+            if (outcomeMessage) {
+                if (outcomeMessage.includes("win")) { 
+                    result.winner = "human";
+                    humanScore++;
+                } else if (outcomeMessage.includes("lose")) {
+                    result.winner = "computer";
+                    computerScore++;
+                };
+
+                return result;
+
+            };
+        };
+
+        while (humanScore < maxRounds && computerScore < maxRounds) {
+
+            const humanSelection = prompt("Enter your choice (rock, paper or scissors): ")
+            
+
+            const roundResult = playRound(humanSelection, computerSelection);
+            roundsPlayed++;
+
+            if (roundResult.winner == "human") {
+                humanScore++;
+            } else if (roundResult.winner == "computer") {
+                computerScore++;
+            }
+        
+            return {
+                humanScore,
+                computerScore,
+                roundsPlayed
+            };
+};
+};
+
+
+const gameResults = playGame();
+
+console.log("Final Score: ");
+console.log("Human Score: ", gameResults.humanScore);
+console.log("Computer Score: ", gameResults.computerScore);
+console.log("Total rounds played: ", gameResults.roundsPlayed);
